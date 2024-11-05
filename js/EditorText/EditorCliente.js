5c6b605519407e986bb654acc97ca531425f3df5
amgomD <108684692+amgomD@users.noreply.github.com>
2024-11-04 21:24:11 -0500

    Command combinado con prototype

--- js/EditorText/EditorCliente.js
+++ js/EditorText/EditorCliente.js
@@ -1,10 +1,6 @@
 
-
-configurarComandos();
-
-function configurarComandos(){
-// Cliente
-const editor = new Editor(document.getElementById('editor'));
+const editor = new Editor(editortext);
+const history = new CommandHistory();
 
 // Configurar comandos y botones
 const commands = [
@@ -17,7 +13,6 @@ const commands = [
     { command: 'insertUnorderedList', selector: 'button.insertUnorderedList' },
     { command: 'insertOrderedList', selector: 'button.insertOrderedList' },
     { command: 'bold', selector: 'button.bold' },
-    { command: 'insertImage', selector: 'button.image' }, // Cambiado a 'insertImage' para evitar duplicación
     { command: 'formatBlock', arg: 'H1', selector: 'button.formatBlockH1' },
     { command: 'formatBlock', arg: 'H2', selector: 'button.formatBlockH2' },
     { command: 'formatBlock', arg: 'H3', selector: 'button.formatBlockH3' },
@@ -27,39 +22,121 @@ const commands = [
     { command: 'cut', selector: 'button.cut' },
   ];
 
-
-commands.forEach(({ command, arg, selector }) => {
-  const buttonElement = document.querySelector(selector);
-  if (buttonElement) {
-    const commandInstance = new Command(editor, command, arg);
-    new ButtonInvoker(buttonElement, commandInstance);
-  }
-});
-
-// Configurar comando con argumento
-const colorInput = document.querySelector('input.input2');
-if (colorInput) {
-  colorInput.addEventListener('change', () => {
-    const colorCommand = new Command(editor, 'foreColor', colorInput.value);
-    colorCommand.execute();
-  });
-}
-
-const fontSizeSelect = document.querySelector('select.fontSizeSelect');
-if (fontSizeSelect) {
-  fontSizeSelect.addEventListener('change', () => {
-    const fontSizeCommand = new Command(editor, 'fontSize', fontSizeSelect.value);
-    fontSizeCommand.execute();
-  });
-}
-const createLinkButton = document.querySelector('button.createLink');
-if (createLinkButton) {
-  createLinkButton.addEventListener('click', () => {
-    const url = prompt('Enter a URL', 'http://');
-    if (url) {
-      const createLinkCommand = new Command(editor, 'createLink', url);
-      createLinkCommand.execute();
+  commands.forEach(({ command, arg, selector }) => {
+    const buttonElement = document.querySelector(selector);
+    if (buttonElement) {
+      const commandInstance = new CommandArgs(editor, command, arg);
+      new ButtonInvoker(buttonElement, commandInstance,history);
     }
   });
-}
-}
+  
+  // Configurar comando con argumento para el input de color
+  const colorInput = document.querySelector('input.input2');
+  if (colorInput) {
+    colorInput.addEventListener('change', () => {
+      const colorCommand = new CommandArgs(editor, 'foreColor', colorInput.value);
+      colorCommand.execute();
+      history.addCommand(new CommandPrototype(colorCommand));
+    });
+  }
+  
+  // Configurar comando con argumento para el select de tamaño de fuente
+  const fontSizeSelect = document.querySelector('select.fontSizeSelect');
+  if (fontSizeSelect) {
+    fontSizeSelect.addEventListener('change', () => {
+      const fontSizeCommand = new CommandArgs(editor, 'fontSize', fontSizeSelect.value);
+      fontSizeCommand.execute();
+      history.addCommand(new CommandPrototype(fontSizeCommand));
+
+    });
+  }
+  
+  // Configurar comando con argumento para crear enlace
+  const createLinkButton = document.querySelector('button.createLink');
+  if (createLinkButton) {
+    createLinkButton.addEventListener('click', () => {
+      const url = prompt('Ingresa una URL', '');
+      if (url) {
+        const createLinkCommand = new CommandArgs(editor, 'createLink', url);
+        createLinkCommand.execute();
+        history.addCommand(new CommandPrototype(createLinkCommand));
+      }
+    });
+  }
+  
+  // Configurar comando para insertar imagen
+  const insertImageButton = document.querySelector('button.image');
+  if (insertImageButton) {
+    insertImageButton.addEventListener('click', () => {
+      const src = prompt('Ingresa la url de la imgaen', '');
+      if (src) {
+        const insertImageCommand = new CommandImg(editor, src);
+        insertImageCommand.execute();
+        history.addCommand(new CommandPrototype(insertImageCommand));
+      }
+    });
+  }
+
+
+  
+
+const menuButton = document.getElementById('historial');
+menuButton.addEventListener('click', mostrarMenuhisto);
+let menuhisto = document.getElementById('sidemenuhisto');
+    // Función para mostrar el menú lateral
+    function mostrarMenuhisto() {
+   let directorio = document.getElementById('directorio');
+      menuhisto.style.right = "0"; // Desliza el menú hacia la vista
+
+
+
+       let conten = `            
+       <h2 class="notifications-title">Cambios realizados</h2>
+          `;
+   
+
+
+           const commands = history.getHistory();
+           commands.forEach(command => {
+            const { command: commandName, arg } = command;
+    
+            conten += `
+               <div class="caja-cambio">
+            <div class="notification">
+               <div class="notification-header">
+                   <div class="user-info">
+                       <img src="img/perfil.webp" width="30px" alt="">
+                       <span>Editaste</span>
+                   </div>
+                   <span id="fechaedit" class="notification-time">Hace un momento  - ${directorio.textContent}</span>
+               </div>
+            
+               <div class="status-change">
+                 <span id="commando" class="status-badge created">${commandName}</span>
+                 <span class="status-arrow">→</span>
+                 <span id="arg" class="status-badge pending">${arg}</span>
+               </div>
+           </div>
+           </div> 
+           `;
+           });
+           menuhisto.innerHTML = conten;
+  }
+
+  // Función para ocultar el menú lateral
+  function ocultarMenuhisto() {
+    menuhisto.style.right = "-590px"; // Desliza el menú hacia fuera de la vista
+  }
+
+  // Mostrar el menú cuando el cursor está sobre el ícono o el menú
+  menuButton.addEventListener("mouseenter", mostrarMenuhisto);
+
+   
+  menuhisto.addEventListener("mouseleave", function() {
+    // Retrasar un poco para dar tiempo al usuario de mover el cursor al ícono
+    setTimeout(function() {
+        if (!menuhisto.matches(':hover') && !menuButton.matches(':hover')) {
+          ocultarMenuhisto();
+        }
+    }, 200);
+});
--- js/clases/Editor.js
+++ js/clases/Editor.js
@@ -1,36 +1,120 @@
-// Receptor
 class Editor {
-    constructor(editorElement) {
-      this.editorElement = editorElement;
-    }
-  
-    executeCommand(command, arg) {
-        editortext.document.execCommand(command, false, arg);
+  constructor(editorElement) {
+    this.editorElement = editorElement;
+  }
+
+  executeCommand(command, arg) {
+    this.editorElement.document.execCommand(command, false, arg);
+  }
+
+  insertImage(src) {
+    const img = document.createElement("img");
+    img.src = src;
+    this.editorElement.document.execCommand("insertHTML", false, img.outerHTML);
+  }
+}
+
+
+// prototype------------------------
+
+class CommandPrototype {
+  constructor(proto) {
+    this.proto = proto;
+    return this.clone();
+  }
+
+  clone() {
+    let command = new Command(this.proto.receiver);
+    command.command = this.proto.command;
+    command.arg = this.proto.arg;
+    command.src = this.proto.src;
+    command.execute = this.proto.execute.bind(command);
+    return command;
+  }
+}
+//--------------------------------------
+
+
+
+class Command {
+  constructor(receiver) {
+    this.receiver = receiver;
+  }
+
+  execute() {
+    throw new Error('Method "execute()" must be implemented.');
+  }
+
+}
+
+class CommandArgs extends Command {
+  constructor(receiver, command, arg = null) {
+    super(receiver);
+    this.command = command;
+    this.arg = arg;
+  }
+
+  execute() {
+    this.receiver.executeCommand(this.command, this.arg);
+  }
+
+}
+
+class CommandImg extends Command {
+  constructor(receiver, src) {
+    super(receiver);
+    this.src = src;
+  }
+
+  execute() {
+    this.receiver.insertImage(this.src);
+  }
+
+}
+
+class ButtonInvoker {
+  constructor(buttonElement, command, history) {
+    this.buttonElement = buttonElement;
+    this.command = command;
+    this.history = history;
+    this.buttonElement.addEventListener("click", () => this.executeCommand());
+  }
+
+  executeCommand() {
+    this.command.execute();
+    this.history.addCommand(new CommandPrototype(this.command));
+  }
+}
+
+//=-------------------- historico --------------
+
+class CommandHistory {
+  constructor() {
+    this.history = [];
+  }
+
+  addCommand(command) {
+    this.history.push(command);
+
+  }
+  getHistory() {
+    return this.history;
+  }
+  /*
+
+  undo() {
+    if (this.history.length > 0) {
+      const lastCommand = this.history.pop();
+      // Aquí puedes implementar la lógica para deshacer el comando
+      console.log("Undo command:", lastCommand);
     }
   }
-  
-  // Comando
-  class Command {
-    constructor(receiver, command, arg = null) {
-      this.receiver = receiver;
-      this.command = command;
-      this.arg = arg;
+
+  redo() {
+    if (this.history.length > 0) {
+      const lastCommand = this.history.pop();
+      lastCommand.execute();
+      this.history.push(lastCommand);
     }
-  
-    execute() {
-      this.receiver.executeCommand(this.command, this.arg);
-    }
-  }
-  
-  // Invocador
-  class ButtonInvoker {
-    constructor(buttonElement, command) {
-      this.buttonElement = buttonElement;
-      this.command = command;
-      this.buttonElement.addEventListener('click', () => this.executeCommand());
-    }
-  
-    executeCommand() {
-      this.command.execute();
-    }
-  }
+  }*/
+}
